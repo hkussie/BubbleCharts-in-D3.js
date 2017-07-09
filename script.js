3db3c20101870a51cc1017a39efd0a82b85be9fb
hkussie <hkussie@gmail.com>
2017-07-09 01:40:24 -0700

    re-structuring script.js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-var app = angular.module("myApp", []);
+(function () {
+	var width = 500;
+	var height = 500;
 
-app.controller('msgController', function($scope) {
-	$scope.message = "Land Use Statistics Map";
-});
+	var svg = d3.select("#chart")
+	   .append("svg")
+	   .attr("height", height)
+	   .attr("width", width)
+	   .append("g")
+	   .attr("transform", "translate(0,0)")
+
+	d3.queue()
+	  .defer(d3.csv, "DefenseSpending.json")
+	  .await(ready)   
+
+	function ready(error, datapoints) {
+
+		var circles = svg.select(".Country")
+		  .data(datapoints)
+		  .enter().append("circle")
+		  .attr("class", "Country") 
+	}  
+
+})();
