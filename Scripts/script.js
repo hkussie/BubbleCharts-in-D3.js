e1a8f615de20bbff77c19dde476ff455659166b5
hkussie <hkussie@gmail.com>
2017-07-16 01:15:47 -0700

    Add css files

---
+++ Scripts/script.js
@@ -0,0 +1,15 @@
+//Implemtation of the require function
+//Allows for the seperation of javascript files, 
+//while still being able to use methods defined 
+//in other javascript files //
+
+require()
+var method = {};
+
+method.message = function() {
+	console.log("Hello there");
+}
+
+method.favorite = function() {
+	console.log();
+}
