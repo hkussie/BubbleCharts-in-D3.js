6d829a4ad86868249c18e4cbb96dda352c2d704f
hkussie <hkussie@gmail.com>
2017-07-16 22:29:30 -0700

    Chanes to main script file

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,14 +2,21 @@
 //Allows for the seperation of javascript files, 
 //while still being able to use methods defined 
 //in other javascript files //
-
-require()
-var method = {};
+var methods = {};
 
 method.message = function() {
 	console.log("Hello there");
 }
 
 method.favorite = function() {
-	console.log();
+	console.log("Node is my favorite!");
 }
+
+method.node = function() {
+	console.log("I Love Node!");
+}
+
+//Exports certain types of data to controller1.js 
+//file. 
+
+exports.data = methods; 
