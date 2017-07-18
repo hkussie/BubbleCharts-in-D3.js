3cddff588ee1d48e5c4861bd993e4c9e0007f2be
hkussie <hkussie@gmail.com>
2017-07-17 23:25:25 -0700

    changes to main script file

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,12 +2,11 @@
 //Allows for the seperation of javascript files, 
 //while still being able to use methods defined 
 //in other javascript files //
-var response = require('./script2.js');
+var response = require('./methods.js');
 
 //Using console.log won't display the any particular data 
 //will return the functions: updateServer, loveNode, and 
 //saySomething
 //console.log(response);
 
-response.data.updateServer();
-response.output;
+response.data.checkCheck();
