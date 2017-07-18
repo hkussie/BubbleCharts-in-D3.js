6182d36aff369d61e71326e8e5ed842d1fad5c7e
hkussie <hkussie@gmail.com>
2017-07-17 23:44:38 -0700

    add methods.js

---
+++ Scripts/methods.js
@@ -0,0 +1,11 @@
+var methods = {};
+
+methods.updateServer = function() {
+	console.log("Important function");
+}
+
+methods.checkCheck = function() {
+	console.log("Lets check this out");
+}
+
+exports.data = methods;
