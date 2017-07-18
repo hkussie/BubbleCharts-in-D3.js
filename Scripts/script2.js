5f01ad895b4541308357006b5680dfdfa1d24b7a
hkussie <hkussie@gmail.com>
2017-07-17 23:37:09 -0700

    add to second script file

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -1,18 +1,1 @@
-var methods = {};
 
-var output = "Testing Baby";
-
-methods.updateServer = function() {
-	console.log("Important function");
-};
-
-methods.addNumber= function(a, b) { 
-	return a + b;
-}
-
-methods.circleCircumference = function(radius) {
-	return 2 + Math.PI * radius;
-}
-
-exports.data = methods;
-exports.output = output;
