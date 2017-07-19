e870e1772b7071f4b251e1c7552f515e6bddf6fc
hkussie <hkussie@gmail.com>
2017-07-18 22:47:39 -0700

    Additions to script2.js

--- Scripts/script2.js
+++ Scripts/script2.js
@@ -1,12 +1,20 @@
 //http is a module within node 
-var server = require('http');
+var http = require('http');
+var server = http.createServer(engine);
 
-server.createServer(engine).listen(1337);
+
+
+server.listen(1337, function() {
+	console.log('Server was hit with a request');
+
+});
 
 function engine(request, response) {
 
+	//console.log(response);
+
 	response.writeHead(200, 'Content-Type: text/plain');
-	response.end('Oh man, we out here!');
+	response.end('Oh man, we out here!' + request.url); 
 
 }
 
